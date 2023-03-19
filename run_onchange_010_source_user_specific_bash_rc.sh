7956e1c5f4e478824cc3f014b25f53456c055c17
quentincaffeino <sergey95zu@gmail.com>
2023-03-19 20:08:05 +0100

    source user specific bash rc

---
+++ run_onchange_010_source_user_specific_bash_rc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ex
+
+function main {
+	local RC_FILENAME="bashrc"
+	local RCD_PATH="~/.$RC_FILENAME.d"
+	local RC_PATH="$HOME/.$RC_FILENAME"
+
+	if [[ $(grep -q ${RCD_PATH} ${RC_PATH}; printf $?) != 0 ]]; then
+		echo 'current bashrc does not source user specific aliases and functions, appending...'
+		cat >> "$RC_PATH" <<EOF
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+	for rc in ~/.bashrc.d/*; do
+		if [ -f "\$rc" ]; then
+			. "\$rc"
+		fi
+	done
+fi
+
+unset rc
+EOF
+	fi
+}
+
+main "$@"
